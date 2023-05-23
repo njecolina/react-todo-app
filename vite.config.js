6c1463c83d0087b93adf66dc470f4ddcfb76c4af
njecolina <abergazpunk@gmail.com>
2023-05-23 03:03:07 +0200

    production 3

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,43 @@
 import { defineConfig } from "vite";
+import { createVuePlugin } from "vite-plugin-vue2";
 import react from "@vitejs/plugin-react-swc";
-import { copyFileSync } from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      "/.nojekyll": {
+        target: "https://njecolina.github.io",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/\.nojekyll/, ""),
+      },
+    },
+    hmr: {
+      protocol: "ws",
+    },
+    fs: {
+      strict: false,
+    },
+    cors: true,
+    force: true,
+    hmrWarning: true,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    disableHostCheck: true,
+    strictPort: false,
+    fsServe: {
+      root: ".",
+      allow: ["."],
+    },
     mimeTypes: {
-      "text/javascript": ["jsx"], // Dodana konfiguracija za .jsx datoteke
+      "text/javascript": ["js", "mjs"],
+      "text/css": "css",
+      "text/html": ["html", "htm"],
+      "application/javascript": ["jsx"],
     },
   },
   build: {
@@ -15,13 +45,6 @@ export default defineConfig({
       output: {
         manualChunks: undefined,
       },
-      afterWriteBundle: [
-        async (outputOptions, bundle, isWrite) => {
-          if (isWrite && outputOptions.dir === "dist") {
-            await copyFileSync(".nojekyll", "dist/.nojekyll");
-          }
-        },
-      ],
     },
   },
 });
