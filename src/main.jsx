72057d80bc11636d55380972340e5e720b197b9d
njecolina <abergazpunk@gmail.com>
2023-05-23 14:40:42 +0200

    testing React To Do App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const root = document.querySelector("#app");
+
+ReactDOM.createRoot(root).render(<App />);
--- vite.config.js
+++ vite.config.js
@@ -3,45 +3,20 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [reactRefresh()],
-  server: {
-    proxy: {
-      "/.nojekyll": {
-        target: "https://njecolina.github.io",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/\.nojekyll/, ""),
-      },
-    },
-    hmr: {
-      protocol: "ws",
-      injectHost: true,
-    },
-    fs: {
-      strict: false,
-    },
-    cors: true,
-    force: true,
-    hmrWarning: true,
-    watch: {
-      usePolling: true,
-      interval: 100,
-    },
-    disableHostCheck: true,
-    strictPort: false,
-    fsServe: {
-      root: ".",
-      allow: ["."],
-    },
-    mimeTypes: {
-      "text/jsx": ["js"],
-    },
-  },
   build: {
+    base: "/",
     rollupOptions: {
       input: {
         main: "./src/main.jsx",
         reactdemo: "./reactdemo.html",
+        index: "./index.html",
       },
     },
+    esbuild: {
+      jsxInject: `import React from 'react';`,
+    },
+  },
+  server: {
+    // Uklonite postavku headers iz konfiguracije servera
   },
 });
