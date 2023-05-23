5f49102b143b1951b6202f6711aa9ea3a6900f0e
njecolina <abergazpunk@gmail.com>
2023-05-23 02:36:05 +0200

    production 3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { copyFileSync } from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+      afterWriteBundle: [
+        async (outputOptions, bundle, isWrite) => {
+          if (isWrite && outputOptions.dir === "dist") {
+            await copyFileSync(".nojekyll", "dist/.nojekyll");
+          }
+        },
+      ],
+    },
+  },
 });
