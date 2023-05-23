edf589dd2200c66c716e94a42e633b737c21fd98
njecolina <abergazpunk@gmail.com>
2023-05-23 13:50:10 +0200

    Updates

--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 export function NewTodoForm({ onSubmit }) {
   const [newItem, setNewItem] = useState("");
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-ReactDOM.render( /*#__PURE__*/React.createElement(React.StrictMode, null, /*#__PURE__*/React.createElement(App, null)), document.getElementById("root"));
+
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
