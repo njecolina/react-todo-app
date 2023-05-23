2cf4639cbaaa2cabbb0e49338acd27b9632b7555
njecolina <59809135+njecolina@users.noreply.github.com>
2023-05-23 14:59:00 +0200

    Delete src directory

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-const root = document.querySelector("#app");
-
-ReactDOM.createRoot(root).render(<App />);
